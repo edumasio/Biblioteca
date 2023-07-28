300f83b03bbf53d766c0ba3121e580be446a90e8
EDUARDO DAMASIO CARNEIRO DE OLIVEIRA <eduardo.dcoliveira@senacsp.edu.br>
2023-07-28 17:23:48 -0300

    tudo do 0

---
+++ Biblioteca/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Biblioteca.Models
+{
+	[Table("Author")]
+	public class Author
+	{
+		public int Id { get; set; }
+		[Required]
+		[StringLength(50)]
+		[Column("AuthorName")]
+		public string? Name { get; set; }
+		
+	}
+}
